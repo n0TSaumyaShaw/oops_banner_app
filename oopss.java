f398f61840f755322b775ab18cc142206ff03003
n0TSaumyaShaw <saumyashaw@outlook.com>
2026-02-17 09:38:54 +0530

    Use case 1 simple java Program

---
+++ oopss.java
@@ -0,0 +1,14 @@
+public class oopss{
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    ******    ***** ");
+        System.out.println("*     *  *     *   *     *  *     *");
+        System.out.println("*     *  *     *   *     *  *      ");
+        System.out.println("*     *  *     *   ******    *****  ");
+        System.out.println("*     *  *     *   *              * ");
+        System.out.println("*     *  *     *   *        *     * ");
+        System.out.println(" *****    *****    *         *****  ");
+
+    }
+}
