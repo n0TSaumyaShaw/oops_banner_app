4974fca77e86e7e9a7fc2fe1379ebfbcdbdccf21
n0TSaumyaShaw <saumyashaw@outlook.com>
2026-02-23 00:51:06 +0530

    used array

--- oopsbanner.java
+++ oopsbanner.java
@@ -1,56 +1,60 @@
-
 public class oopsbanner {
 
     public static void main(String[] args) {
 
+        String[] lines = new String[7];
 
-        System.out.println(String.join(" ",
+        lines[0] = String.join(" ",
                 "  *****  ",
                 "  *****  ",
                 "  ****** ",
                 "  ****** "
-        ));
+        );
 
-        System.out.println(String.join(" ",
+        lines[1] = String.join(" ",
                 " **     **",
                 " **     **",
                 " **     **",
                 " **      "
-        ));
+        );
 
-        System.out.println(String.join(" ",
+        lines[2] = String.join(" ",
                 " **     **",
                 " **     **",
                 " **     **",
                 " **      "
-        ));
+        );
 
-        System.out.println(String.join(" ",
+        lines[3] = String.join(" ",
                 " **     **",
                 " **     **",
                 " ******  ",
                 "  *****   "
-        ));
+        );
 
-        System.out.println(String.join(" ",
+        lines[4] = String.join(" ",
                 " **     **",
                 " **     **",
                 " **       ",
                 "       ** "
-        ));
+        );
 
-        System.out.println(String.join(" ",
+        lines[5] = String.join(" ",
                 " **     **",
                 " **     **",
                 " **       ",
                 "       ** "
-        ));
+        );
 
-        System.out.println(String.join(" ",
+        lines[6] = String.join(" ",
                 "  *****  ",
                 "  *****  ",
                 " **       ",
                 "  ****** "
-        ));
+        );
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
